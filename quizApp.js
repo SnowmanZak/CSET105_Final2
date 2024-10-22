0c39b50841d27341a7af158d00cc2fc5ac07e9dc
KalliPutt <kalli.putt.5.23.07@gmail.com>
2024-10-22 08:37:24 -0400

    added a background to the score page.

--- quizApp.js
+++ quizApp.js
@@ -3,7 +3,53 @@ const quizData = [
         question: "What is the capital of France?",
         options: ["Berlin", "Madrid", "Paris", "Lisbon"],
         correct: 2 
-    }
+    },
+    {   //Question 2
+        question: "Which language is used for web apps?",
+        options: ["Python", "JavaScript", "Java", "C++"],
+        correct: 1
+    },
+    {   //Question 3
+        question: "Who is the founder of Microsoft?",
+        options: ["Steve Jobs", "Bill Gates", "Elon Musk", "Mark Zuckerberg"],
+        correct: 1
+    },
+    {   //Question 4
+        question: "What is 9 + 10?",
+        options: ["20","21","19","18"],
+        correct: 2
+    },
+    {   //Question 5
+        question: "What is the chemical symbol for water?",
+        options: ["H2O", "O2", "CO2", "H2"],
+        correct: 0
+    },
+    {   //Question 6
+        question: "What British prison colony became a country?",
+        options: ["France", "United States of America", "India", "Australia"],
+        correct: 3
+    },
+    {   //Question 7
+        question: "What is the hardest natural substance on Earth?",
+        options: ["Gold", "Diamond", "Iron", "Quartz"],
+        correct: 1
+    },
+    {   //Question 8
+        question: "When the Boogeyman goes to sleep, he checks his closet for Chuck Norris.",
+        options: [
+            "True", "False", "Only on Halloween", "In dreams"],
+        correct: 0
+    },
+    {   //Question 9
+        question: "Which of the Seven Wonders of the ANCIENT World still remain to this day?",
+        options: ["Colossus of Rhodes", "Lighthouse of Alexanderia", "Great Pyramid of Giza", "Hanging Gardens of Babylon"],
+        correct: 2
+    },
+    {   //Question 10
+        question: "Who is the best pass rusher in the NFL?",
+        options: ["Micah Parsons", "TJ Watt", "Myles Garrett", "Nick Bosa"],
+        correct: 1
+    },
 ];
 
 let currentQuestionIndex = 0;
